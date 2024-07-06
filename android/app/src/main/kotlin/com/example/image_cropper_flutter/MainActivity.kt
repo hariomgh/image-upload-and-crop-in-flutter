0d9125953036cc5cc67a9773236d934fd6ccfa58
Hariom Vishwkarma <2107191548001@axiscolleges.in>
2024-07-06 18:06:43 +0530

    image upload and crop

---
+++ android/app/src/main/kotlin/com/example/image_cropper_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.image_cropper_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,205 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Cropper Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  File? imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20.0,
+            ),
+            imageFile == null
+                ? ClipRRect(
+                    borderRadius: BorderRadius.circular(150.0),
+                    child: Image.asset(
+                      'assets/default_profile.png',
+                      height: 300.0,
+                      width: 300.0,
+                    ))
+                : ClipRRect(
+                    borderRadius: BorderRadius.circular(150.0),
+                    child: Image.file(
+                      imageFile!,
+                      height: 300.0,
+                      width: 300.0,
+                      fit: BoxFit.fill,
+                    )),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                Map<Permission, PermissionStatus> statuses = await [
+                  Permission.storage,
+                  Permission.camera,
+                ].request();
+                if (statuses[Permission.storage]!.isGranted &&
+                    statuses[Permission.camera]!.isGranted) {
+                  showImagePicker(context);
+                } else {
+                  print('no permission provided');
+                }
+              },
+              child: Text('Select Image'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  final picker = ImagePicker();
+
+  void showImagePicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (builder) {
+          return Card(
+            child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 5.2,
+                margin: const EdgeInsets.only(top: 8.0),
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        child: InkWell(
+                      child: Column(
+                        children: const [
+                          Icon(
+                            Icons.image,
+                            size: 60.0,
+                          ),
+                          SizedBox(height: 12.0),
+                          Text(
+                            "Gallery",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                          )
+                        ],
+                      ),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.pop(context);
+                      },
+                    )),
+                    Expanded(
+                        child: InkWell(
+                      child: SizedBox(
+                        child: Column(
+                          children: const [
+                            Icon(
+                              Icons.camera_alt,
+                              size: 60.0,
+                            ),
+                            SizedBox(height: 12.0),
+                            Text(
+                              "Camera",
+                              textAlign: TextAlign.center,
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.black),
+                            )
+                          ],
+                        ),
+                      ),
+                      onTap: () {
+                        _imgFromCamera();
+                        Navigator.pop(context);
+                      },
+                    ))
+                  ],
+                )),
+          );
+        });
+  }
+
+  _imgFromGallery() async {
+    await picker
+        .pickImage(source: ImageSource.gallery, imageQuality: 50)
+        .then((value) {
+      if (value != null) {
+        _cropImage(File(value.path));
+      }
+    });
+  }
+
+  _imgFromCamera() async {
+    await picker
+        .pickImage(source: ImageSource.camera, imageQuality: 50)
+        .then((value) {
+      if (value != null) {
+        _cropImage(File(value.path));
+      }
+    });
+  }
+
+  _cropImage(File imgFile) async {
+    final croppedFile = await ImageCropper().cropImage(
+      sourcePath: imgFile.path,
+      aspectRatio:
+          CropAspectRatio(ratioX: 1, ratioY: 1), // Set the aspect ratio to 1:1
+      aspectRatioPresets: Platform.isAndroid
+          ? [
+              CropAspectRatioPreset.square,
+            ]
+          : [
+              CropAspectRatioPreset.square,
+            ],
+      uiSettings: [
+        AndroidUiSettings(
+          toolbarTitle: "Image Cropper",
+          toolbarColor: Colors.deepOrange,
+          toolbarWidgetColor: Colors.white,
+          initAspectRatio: CropAspectRatioPreset.square,
+          lockAspectRatio: true, // Lock the aspect ratio to 1:1
+        ),
+        IOSUiSettings(
+          title: "Image Cropper",
+          aspectRatioLockEnabled: true, // Lock the aspect ratio to 1:1
+        )
+      ],
+    );
+    if (croppedFile != null) {
+      imageCache.clear();
+      setState(() {
+        imageFile = File(croppedFile.path);
+      });
+      // reload();
+    }
+  }
+}
